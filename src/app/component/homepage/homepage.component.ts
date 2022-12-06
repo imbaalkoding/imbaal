f83979949b28a105d991e3be44779c7701656a21
imbaalkoding <muhamadbalkoding98@gmail.com>
2022-12-06 21:04:41 +0700

    adding maps

--- src/app/component/homepage/homepage.component.ts
+++ src/app/component/homepage/homepage.component.ts
@@ -8,12 +8,17 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HomepageComponent implements OnInit {
   @Input('iniHp')
   public isMobile :any
+  title = 'My first AGM project';
+  lat = 51.678418;
+  lng = 7.809007;
 
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  // images = [1, 2, 3].map((n) => `../../../assets/frame${n}`);
   panelOpenState = false;
   constructor() { }
   ngOnInit(): void {
+
   }
+
   
 
 
