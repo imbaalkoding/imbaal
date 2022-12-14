b73c4eb2ad5c343ab9ae2661258484801c6d597e
imbaalkoding <muhamadbalkoding98@gmail.com>
2022-12-14 07:36:25 +0700

    perubahan mobile

--- src/app/component/ourservis/ourservis.component.ts
+++ src/app/component/ourservis/ourservis.component.ts
@@ -12,17 +12,33 @@ export class OurservisComponent implements OnInit{
 @Input('iniHp')
 public isMobile:any;
 panelOpenState =false
+lokasi = [{ tempat:"Perumahan \nSerpong green paradise",
+            foto: "../../../assets/foto/bor1.jpg" },
+          {tempat:"Perumahan \nPuri harmoni 8",
+          foto: "../../../assets/foto/bor2.jpg"},
+          {tempat:"Perumahan \nPuri harmoni 8",
+          foto: "../../../assets/foto/bor3.jpg"},
+          {tempat:"Perumahan \nmetro parungpanjang 1",
+          foto: "../../../assets/foto/bor4.jpg"},
+          {tempat:"Perumahan \nmetro parungpanjang 2",
+          foto: "../../../assets/foto/bor5.jpg"},
+          {tempat:"Tangerang Selatan",
+          foto: "../../../assets/foto/bor6.jpg"},
+          {tempat:"Tengerang kota",
+          foto: "../../../assets/foto/bor7.jpg"} ]
 showNavigationArrows = true;
 showNavigationIndicators = false;
 images = [1,2,3,4,5,6,7].map((n) => `../../../assets/foto/bor${n}.jpg `);
-videos = [1,2,3,4,5,6,7].map((n)=>  `../../../assets/video/bor${n}.mp4`)
+videos = [1,2,3,4,5,6,7].map((n)=>  `../../../assets/video/bor${n}.mp4`);
+
+
   constructor(config: NgbCarouselConfig) {
     config.showNavigationArrows = true;
     config.showNavigationIndicators = false;
    }
-  
+ 
   ngOnInit(): void {
-    console.log(this.images[1]) 
+    console.log(this.lokasi) 
   }
 
 }
